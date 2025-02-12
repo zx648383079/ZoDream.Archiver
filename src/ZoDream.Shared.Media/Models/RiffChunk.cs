3a8aeac7530e6c429271f652e464432cdb4623ff
ZoDream <648383079@qq.com>
2025-02-12 16:12:29 +0800

    update

---
+++ src/ZoDream.Shared.Media/Models/RiffChunk.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ZoDream.Shared.Media.Models
+{
+    public interface IRiffChunk
+    {
+        public string Id { get; }
+    }
+
+    public class RiffChunk : IRiffChunk
+    {
+        public string Id { get; set; }
+
+        public uint Length { get; set; }
+
+        public string Type { get; set; }
+
+        public IList<IRiffChunk> Items { get; set; } = [];
+    }
+
+    public class RiffSubChunk : IRiffChunk
+    {
+        public string Id { get; set; }
+
+        public uint Length { get; set; }
+
+        public long Position { get; set; }
+    }
+}
---
+++ src/ZoDream.Shared.Media/RiffReader.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using ZoDream.Shared.Media.Models;
+
+namespace ZoDream.Shared.Media
+{
+    public class RiffReader
+    {
+
+
+        public RiffChunk Read(Stream input)
+        {
+            var pos = input.Position;
+            var reader = new BinaryReader(input);
+            var magic = Encoding.ASCII.GetString(reader.ReadBytes(4));
+            Debug.Assert(magic == "RIFF");
+            input.Position = pos;
+            return (RiffChunk)Read(reader);
+        }
+
+        private IRiffChunk Read(BinaryReader reader)
+        {
+            var magic = Encoding.ASCII.GetString(reader.ReadBytes(4));
+            var length = reader.ReadUInt32();
+            if (magic != "RIFF" && magic != "LIST")
+            {
+                var chunk = new RiffSubChunk()
+                {
+                    Id = magic,
+                    Length = length,
+                    Position = reader.BaseStream.Position
+                };
+                reader.BaseStream.Seek(chunk.Length, SeekOrigin.Current);
+                return chunk;
+            }
+            {
+                var chunk = new RiffChunk()
+                {
+                    Id = magic,
+                    Length = length,
+                    Type = Encoding.ASCII.GetString(reader.ReadBytes(4)),
+                };
+                var end = reader.BaseStream.Position + length;
+                while (reader.BaseStream.Position < end)
+                {
+                    chunk.Items.Add(Read(reader));
+                }
+                return chunk;
+            }
+        }
+    }
+}
